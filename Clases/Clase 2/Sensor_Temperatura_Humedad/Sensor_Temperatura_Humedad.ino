2fae66fbdefc5d9340728db18dcc36438fb495f5
lissethmgala <lmerinog@pucp.pe>
2019-01-14 16:11:52 -0500

    Código necesario para el sensor DHT11

--- Clases/Clase 2/Sensor_Temperatura_Humedad/Sensor_Temperatura_Humedad.ino
+++ Clases/Clase 2/Sensor_Temperatura_Humedad/Sensor_Temperatura_Humedad.ino
@@ -1,1 +1,45 @@
+/*
+ * Taller IoT PUCP Verano 2019
+ * Trabajando con un sensor de Temperatura y Humedad 
+ */
 
+//Primero se debe incluir las siguientes librerias del sensor DHT
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+//Se define el tipo de sensor a utilizar, en este caso será el DHT11
+#define DHTTYPE DHT11
+
+//Se define el Pin a utilizar, en este caso será el GPIO 2
+#define dht_dpin 2
+
+//Se crea una variable tipo Objeto DHT y se le pasa los 2 parámetros definidos previamente
+DHT dht(dht_dpin, DHTTYPE); 
+
+//Se inicializa 
+void setup(void)
+{ 
+  dht.begin();
+  Serial.begin(9600);
+  Serial.println("Humedad y Temperatura\n\n");
+  delay(700);
+
+}
+
+//Con esa función se comenzará a capturar los valores de la Temperatura y Humedad
+void loop() {
+  
+    //Cada valor capturado se asigna a una respectiva variable
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();         
+  
+    //Se comienza a imprimir los valores
+    Serial.print("Current humidity = ");
+    Serial.print(h);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(t); 
+    Serial.println("C  ");
+  delay(800);   //Este bucle se repetirá cada 800 ms
+}
