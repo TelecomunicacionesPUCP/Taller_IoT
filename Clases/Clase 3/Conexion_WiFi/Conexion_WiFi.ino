546d07fce2b89713f85741112db672a965668a80
lissethmgala <lmerinog@pucp.pe>
2019-01-18 17:48:16 -0500

    Update Conexion_WiFi.ino

--- Clases/Clase 3/Conexion_WiFi/Conexion_WiFi.ino
+++ Clases/Clase 3/Conexion_WiFi/Conexion_WiFi.ino
@@ -1,23 +1,30 @@
+/* Taller IoT PUCP Verano 2019
+*   CLASE 3: WiFi
+*    ACTIVIDAD: Conexión a una red WiFi
+*/
+
+//Primero se debe agregar la libreria necesaria
 #include <ESP8266WiFi.h>
 
+//Se declara como variables las credenciales de la red WiFi 
 const char* ssid     = "redpucp";
 const char* password = "C9AA28BA93";
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //Se inicializa la comunicación serial
   delay(10);
   
   Serial.print("Conectando a ");
-  Serial.println(ssid);
+  Serial.println(ssid); 
   
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
 
+  //Nos conectamos a la red WiFi
+  WiFi.begin(ssid, password);  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("");
   Serial.println("WiFi conectado");
   Serial.println("Direccion IP: ");
