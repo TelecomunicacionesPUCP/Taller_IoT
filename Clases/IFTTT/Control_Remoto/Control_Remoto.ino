fe84b65d231189e1833a37c3b93389656997eaa9
Alejandro Macedo <a.macedo@pucp.pe>
2019-02-07 02:21:10 -0500

    Update Control_Remoto.ino

--- Clases/IFTTT/Control_Remoto/Control_Remoto.ino
+++ Clases/IFTTT/Control_Remoto/Control_Remoto.ino
@@ -1,3 +1,22 @@
+/* 
+ *  Taller IoT PUCP Verano 2019
+ *  Control Remoto activado por voz con Google Assistant, Blynk y IFTTT
+ *  
+ *  Se usa la libreria IRremoteESP8266 para hacer el envío de los pulsos
+ *  con la codificación necesaria para prender un televisor AOC que usa
+ *  la codificación RC6.
+ *  
+ *  Para decodificar los códigos se usa el archivo de ejemplo IRrecvDumpV2
+ *  dentro de la misma librería.
+ *  
+ *  Se usa Blynk como intermediario entre IFTTT y la placa para su conexión
+ *  mediante la API de Blynk, lo cual nos permite interactuar con la placa solo
+ *  sabiendo su token generado.
+ *  
+ *  IFTTT se encarga de enviar a la placa una actualización a uno de los pines
+ *  virtuales que usa Blynk para que se mande una señal correspondiente.
+ */
+
 // Definición para que se impriman los mensajes de Blynk
 #define BLYNK_PRINT Serial
 
@@ -30,7 +49,7 @@ void setup()
   // Inicializar la instancia de envio IR
   irsend.begin();
 
-  // Se inicia la comunicación con Blynk con los datos dados
+  // Se inicia la comunicación con Blynk con los datos dados  
   Blynk.begin(token, ssid, password);
 }
 
@@ -43,9 +62,53 @@ BLYNK_WRITE(V0) {
 
 // Pin V1 de silenciar
 BLYNK_WRITE(V1) {
-  Serial.println("Se usó el pin V1");
+  Serial.println("Se uso el pin V1");
 
   irsend.sendRC6(0xD, 20, 2); // Se envía la señal D en codificación RC6 repetida 2 veces
+}
+
+// Pin V2 de subir volumen (se tiene como parámetro la cantidad de volumen a subir)
+BLYNK_WRITE(V2) {
+  Serial.println("Se uso el pin V2");
+
+  int veces = param.asInt(); // Cantidad de volumen a subir
+  bool cambio = false; // Bit de cambio (característica de la codificación RC6)
+
+  for (int i = 0; i < veces; i++) {
+    if (cambio) irsend.sendRC6(0x10, 20);
+    else irsend.sendRC6(irsend.toggleRC6(0x10, 20)); Se usa una función de la librería para cambiar la información que se envía
+    cambio = !cambio;
+    delay(200);
+  }
+}
+
+// Pin V3 de bajar volumen (se tiene como parámetro la cantidad de volumen a bajar)
+BLYNK_WRITE(V3) {
+  Serial.println("Se uso el pin V3");
+
+  int veces = param.asInt(); // Cantidad de volumen a subir
+  bool cambio = false; // Bit de cambio (característica de la codificación RC6)
+
+  for (int i = 0; i < veces; i++) {
+    if (cambio) irsend.sendRC6(0x11, 20);
+    else irsend.sendRC6(irsend.toggleRC6(0x11, 20)); // Se usa una función de la librería para cambiar la información que se envía
+    cambio = !cambio;
+    delay(200);
+  }
+}
+
+// Pin V4 de subir canal
+BLYNK_WRITE(V4) {
+  Serial.println("Se uso el pin V4");
+
+  irsend.sendRC6(0x20, 20);
+}
+
+// Pin V5 de bajar canal
+BLYNK_WRITE(V5) {
+  Serial.println("Se usó el pin V5");
+
+  irsend.sendRC6(0x21, 20);
 }
 
 void loop()
